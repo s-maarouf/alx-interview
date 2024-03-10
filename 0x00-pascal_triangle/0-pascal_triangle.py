35c8ca878b9334317fff9f343c52f78614867211
s-maarouf <maarouf.salahedd@gmail.com>
2024-03-10 13:04:19 +0000

    Completed pascal's triangle

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,12 @@
+def pascal_triangle(n: int):
+    if n <= 0:
+        return [{}]
+    else:
+        triangle = [[1]]
+        for i in range(n - 1):
+            row = [1]
+            for j in range(i):
+                row.append(triangle[-1][j] + triangle[-1][j+1])
+            row.append(1)
+            triangle.append(row)
+        return triangle
