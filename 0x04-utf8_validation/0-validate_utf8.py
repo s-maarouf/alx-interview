dde4f5505c0ad199f7c77308412da71ba048c3eb
s-maarouf <maarouf.salahedd@gmail.com>
2024-04-06 00:10:36 +0000

    Implemented task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def validUTF8(data):
+
+    def is_leading_byte(byte):
+        return bin(byte).startswith('0b' + '1' * (8 - num_bytes) + '0')
+
+    num_bytes = 0
+
+    for byte in data:
+        if num_bytes == 0:
+            if byte >> 3 == 0b11110:
+                num_bytes = 3
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 7 == 0b0:
+                num_bytes = 0
+            else:
+                return False
+        else:
+            if not is_leading_byte(byte):
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
